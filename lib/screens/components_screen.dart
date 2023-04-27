d488c698e7556369f59f5205ff7f22428c213290
Yash9013 <yashkatariya2000@gmail.com>
2023-04-27 19:40:28 +0530

    Added Field text

--- lib/screens/components_screen.dart
+++ lib/screens/components_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-RichText appText({required String text}) {
+RichText appText({required String field,required String text}) {
   return RichText(
     text: TextSpan(
       children: [
-        const TextSpan(
-          text: 'Name :-  ',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+         TextSpan(
+          text: '$field :-  ',
+          style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         TextSpan(
           text: text,
